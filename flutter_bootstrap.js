4d69169a60183cfe1b0f2bc234733d1ebfb94e3f
muhammadmnyer <muhammadmnyer@gmail.com>
2024-11-17 12:01:03 +0300

    changing the url strategy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1577692033"
+    serviceWorkerVersion: "41601678"
   }
 });

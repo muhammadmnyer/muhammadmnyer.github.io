f8f49acd4e650450e14504689b17979dcd64bf6a
muhammadmnyer <muhammadmnyer@gmail.com>
2024-11-24 08:14:55 +0300

    built with env variables

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "756617591"
+    serviceWorkerVersion: "2444175280"
   }
 });
